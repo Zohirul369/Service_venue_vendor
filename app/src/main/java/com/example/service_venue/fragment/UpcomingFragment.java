834a17b582f0cd79596903c60df991dc3fdbcf73
Zohirul369 <zohirul369@gmail.com>
2024-02-06 00:21:15 +0600

    Solve error but data not load

--- app/src/main/java/com/example/service_venue/fragment/UpcomingFragment.java
+++ app/src/main/java/com/example/service_venue/fragment/UpcomingFragment.java
@@ -1,79 +1,66 @@
 package com.example.service_venue.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.SearchView;
-
 import com.example.service_venue.R;
 import com.example.service_venue.ViewModel;
 import com.example.service_venue.adapter.UpCommingAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
-
 
 public class UpcomingFragment extends Fragment {
 
-
     RecyclerView recyclerView;
-    UpCommingAdapter upCommingAdapter;
+    UpCommingAdapter upComingAdapter;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_upcoming);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_upcoming, container, false);
 
-
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
         FirebaseRecyclerOptions<ViewModel> options =
                 new FirebaseRecyclerOptions.Builder<ViewModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("request"),ViewModel.class)
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("request"), ViewModel.class)
                         .build();
 
-        upCommingAdapter = new upCommingAdapter(options);
-        recyclerView.setAdapter(upCommingAdapter);
+        upComingAdapter = new UpCommingAdapter(options);
+        recyclerView.setAdapter(upComingAdapter);
 
-
-    }
-
-    private void setContentView(int fragmentUpcoming) {
-
+        return view;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        upCommingAdapter.startListening();
+        upComingAdapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        upCommingAdapter.stopListening();
+        upComingAdapter.stopListening();
     }
 
-
-    private void txtSearch(String str){
+    private void txtSearch(String str) {
         FirebaseRecyclerOptions<ViewModel> options =
                 new FirebaseRecyclerOptions.Builder<ViewModel>()
                         .setQuery(FirebaseDatabase.getInstance()
                                 .getReference().child("request")
                                 .orderByChild("cname")
-                                .startAt(str).endAt(str+"~"), ViewModel.class)
+                                .startAt(str).endAt(str + "~"), ViewModel.class)
                         .build();
-        upCommingAdapter = new upCommingAdapter(options);
-        upCommingAdapter.startListening();
-        recyclerView.setAdapter(upCommingAdapter);
+        upComingAdapter = new UpCommingAdapter(options);
+        upComingAdapter.startListening();
+        recyclerView.setAdapter(upComingAdapter);
     }
 }
