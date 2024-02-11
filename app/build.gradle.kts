7c050686e91a44e84be6846cba2e46b91bb7e167
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-11 10:45:01 +0600

    add firebase for authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,14 @@ dependencies {
     implementation("com.squareup.picasso:picasso:2.5.2")
     implementation("androidx.fragment:fragment:$fragment_version")
 
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
 }
 apply(plugin = "com.google.gms.google-services")
 apply(plugin = "com.android.application")
