cc42c25c5c4d2d85ab6035c00eabec38bc91b408
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-11 11:31:54 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/service_venue/ProfilePrivacy.java
@@ -0,0 +1,14 @@
+package com.example.service_venue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilePrivacy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_privacy);
+    }
+}
