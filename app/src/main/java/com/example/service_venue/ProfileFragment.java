36a79029221375083d27ffd86dba61d191247df0
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-11 11:08:29 +0600

    add onview method

--- app/src/main/java/com/example/service_venue/ProfileFragment.java
+++ app/src/main/java/com/example/service_venue/ProfileFragment.java
@@ -1,5 +1,10 @@
 package com.example.service_venue;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 
@@ -7,9 +12,12 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
+        return inflater.inflate(R.layout.fragment_profile, container, false);
 
-
-
+    }
 
 }
