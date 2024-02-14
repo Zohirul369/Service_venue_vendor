3252fa84083338068e8e9e4485b0d3b816af5a80
Zohirul369 <zohirul369@gmail.com>
2024-02-15 03:13:48 +0600

    update complete fragment, RequestAdapter

--- app/src/main/java/com/example/service_venue/adapter/RequestAdapter.java
+++ app/src/main/java/com/example/service_venue/adapter/RequestAdapter.java
@@ -5,27 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.service_venue.R;
 import com.example.service_venue.ViewModel;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.FirebaseDatabase;
-import com.orhanobut.dialogplus.DialogPlus;
-import com.orhanobut.dialogplus.ViewHolder;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RequestAdapter extends FirebaseRecyclerAdapter<ViewModel, RequestAdapter.myViewHolder> {
@@ -37,7 +24,6 @@ public class RequestAdapter extends FirebaseRecyclerAdapter<ViewModel, RequestAd
     @Override
     protected void onBindViewHolder(@NonNull myViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull ViewModel model) {
         holder.cName.setText(model.getcName());
-        //holder.cWorkTitle.setText(model.getcWorkTitle());
         holder.cDate.setText(model.getcDate());
         holder.cAddress.setText(model.getcAddress());
         holder.serviceName.setText(model.getServiceName());
@@ -67,17 +53,15 @@ public class RequestAdapter extends FirebaseRecyclerAdapter<ViewModel, RequestAd
 
     class myViewHolder extends RecyclerView.ViewHolder {
         CircleImageView img;
-        TextView cName, cWorkTitle, cDate,cAddress,serviceName,address_title,cPhone;
+        TextView cName, cDate,cAddress,serviceName,address_title,cPhone;
 
-        Button view_order, btn_accept, btn_cancel;
-
+        Button  btn_accept, btn_cancel;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
             img = (CircleImageView) itemView.findViewById(R.id.client_img);
             cName = (TextView) itemView.findViewById(R.id.cname);
-            //cWorkTitle = (TextView) itemView.findViewById(R.id.job_task);
             cDate = (TextView) itemView.findViewById(R.id.cdate);
             cAddress = (TextView) itemView.findViewById(R.id.caddress);
             serviceName = (TextView) itemView.findViewById(R.id.job_task);
--- app/src/main/java/com/example/service_venue/fragment/CompletedFragment.java
+++ app/src/main/java/com/example/service_venue/fragment/CompletedFragment.java
@@ -1,55 +1,21 @@
 package com.example.service_venue.fragment;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.service_venue.R;
-import com.example.service_venue.ViewModel;
-import com.example.service_venue.adapter.RequestAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.FirebaseDatabase;
-
 
 public class CompletedFragment extends Fragment {
 
-    private RecyclerView recyclerView;
-    private RequestAdapter requestAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_completed, container, false);
-
-        recyclerView = view.findViewById(R.id.com_recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        FirebaseRecyclerOptions<ViewModel> options =
-                new FirebaseRecyclerOptions.Builder<ViewModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("serviceVenue").child("request"), ViewModel.class)
-                        .build();
-
-        requestAdapter = new RequestAdapter(options);
-        recyclerView.setAdapter(requestAdapter);
-
         return view;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        requestAdapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        requestAdapter.stopListening();
     }
 }
 
