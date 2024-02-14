407067231081a56eaa62661a62e3ca17ba68519f
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-14 14:36:36 +0600

    implemet slider

--- app/src/main/java/com/example/service_venue/Home_Fragment.java
+++ app/src/main/java/com/example/service_venue/Home_Fragment.java
@@ -11,6 +11,9 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.service_venue.service.BeautyParlourService;
 import com.example.service_venue.service.CarRentService;
 import com.example.service_venue.service.CarWashService;
@@ -20,6 +23,8 @@ import com.example.service_venue.service.LondiService;
 import com.example.service_venue.service.MovingService;
 import com.example.service_venue.service.PaintingService;
 import com.example.service_venue.service.PlumbingService;
+
+import java.util.ArrayList;
 
 
 public class Home_Fragment extends Fragment {
@@ -33,6 +38,22 @@ public class Home_Fragment extends Fragment {
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_home, container, false);
         View view = inflater.inflate(R.layout.fragment_home, container,false);
+
+        //slider-----------------------------------------------------
+
+        ArrayList<SlideModel> imageList = new ArrayList<>(); // Create image list
+
+// imageList.add(new SlideModel("String Url" or R.drawable))
+// imageList.add(new SlideModel("String Url" or R.drawable, "title")) You can add title
+
+        imageList.add(new SlideModel(R.drawable.carpenter, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct.",ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.",ScaleTypes.CENTER_CROP));
+
+        ImageSlider imageSlider = view.findViewById(R.id.smartSlider);
+        imageSlider.setImageList(imageList);
+
+        //-------------------------------------------------------------
 
 
         // Service section start
