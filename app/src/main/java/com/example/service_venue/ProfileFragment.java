9e56c557842f7e70117b74aa58fa4d46bd1f067a
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-15 19:49:34 +0600

    inten update

--- app/src/main/java/com/example/service_venue/ProfileFragment.java
+++ app/src/main/java/com/example/service_venue/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.example.service_venue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,12 +14,23 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
+    TextView vendor_view_profile;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        vendor_view_profile = view.findViewById(R.id.vendor_view_profile);
+        vendor_view_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), profile_view.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
-
 }
