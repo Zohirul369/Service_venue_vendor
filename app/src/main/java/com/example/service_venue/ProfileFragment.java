7169b6394d7c281b4732f0af55dd59373c0b3754
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-16 06:13:51 +0600

    contact us, privacy intent

--- app/src/main/java/com/example/service_venue/ProfileFragment.java
+++ app/src/main/java/com/example/service_venue/ProfileFragment.java
@@ -14,15 +14,35 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
-    TextView vendor_view_profile;
+    TextView vendor_view_profile, contact_layout, privacy;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
+        
+        privacy = view.findViewById(R.id.privacy);
         vendor_view_profile = view.findViewById(R.id.vendor_view_profile);
+        contact_layout= view.findViewById(R.id.contact_layout);
+
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ProfilePrivacy.class);
+                startActivity(intent);
+            }
+        });
+        contact_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Profile_Contact.class);
+                startActivity(intent);
+            }
+        });
+
         vendor_view_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
